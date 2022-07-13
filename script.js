81250e696c33e7957f1affc05b1aa889ab55fa2c
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-07-13 18:27:49 -0300

    requisito 3 concluido

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementsByTagName('button')[0];
+const emailInput = document.getElementsByTagName('input')[0];
+const passworldInput = document.getElementsByTagName('input')[1];
+
+function alerta() {
+  button.addEventListener('click', () => {
+    if (
+      emailInput.value === 'tryber@teste.com'
+      && passworldInput.value === '123456'
+    ) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
+}
+alerta();
